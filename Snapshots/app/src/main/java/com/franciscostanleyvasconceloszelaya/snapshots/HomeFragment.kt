c79d3657d782b80faf852971e3969987cd8ff8de
Frank <stanleyvasconcelos0@gmail.com>
2022-04-29 12:20:33 -0600

    feat(snapshot): Add the data class and viewholder

--- Snapshots/app/src/main/java/com/franciscostanleyvasconceloszelaya/snapshots/HomeFragment.kt
+++ Snapshots/app/src/main/java/com/franciscostanleyvasconceloszelaya/snapshots/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.franciscostanleyvasconceloszelaya.snapshots.databinding.ItemSnapshotBinding
 
 class HomeFragment : Fragment() {
 
@@ -20,5 +22,13 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    inner class SnapshotHolder(view: View): RecyclerView.ViewHolder(view){
+        val binding = ItemSnapshotBinding.bind(view)
+
+        fun setListener(snapshot: Snapshot){
+
+        }
     }
 }
