15a72104e3e4d08a812bc6ea928f3af9249dcfe3
Frank <stanleyvasconcelos0@gmail.com>
2022-05-13 10:19:04 -0600

    feat(stores_mvvm): use of lazy to obtain the data of the stores

--- Stores_MVVM/app/src/main/java/com/franciscostanleyvasconceloszelaya/stores/mainModule/viewModel/MainViewModel.kt
+++ Stores_MVVM/app/src/main/java/com/franciscostanleyvasconceloszelaya/stores/mainModule/viewModel/MainViewModel.kt
@@ -7,19 +7,21 @@ import com.franciscostanleyvasconceloszelaya.stores.common.entities.StoreEntity
 import com.franciscostanleyvasconceloszelaya.stores.mainModule.model.MainInteractor
 
 class MainViewModel : ViewModel() {
-    private var stores: MutableLiveData<List<StoreEntity>> = MutableLiveData()
-    private val interactor: MainInteractor = MainInteractor()
+    private val interact: MainInteractor = MainInteractor()
 
-    init {
-        loadStores()
+    private val stores: MutableLiveData<List<StoreEntity>> by lazy {
+        MutableLiveData<List<StoreEntity>>().also {
+            loadStores()
+        }
     }
+
 
     fun getStores(): LiveData<List<StoreEntity>> {
         return stores
     }
 
     private fun loadStores(){
-        interactor.getStoresCallback(object : MainInteractor.StoresCallBack{
+        interact.getStoresCallback(object : MainInteractor.StoresCallBack{
             override fun getStoresCallback(callback: MutableList<StoreEntity>) {
                 stores.value = callback
             }
