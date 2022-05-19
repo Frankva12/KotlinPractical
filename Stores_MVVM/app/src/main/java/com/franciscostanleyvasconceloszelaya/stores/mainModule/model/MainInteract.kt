460a813ff55d448921b5a31f37a712ac7ad0d286
Frank <stanleyvasconcelos0@gmail.com>
2022-05-19 09:24:04 -0600

    feat(stores_mvvm_using_api): get data from json using gson

--- Stores_MVVM/app/src/main/java/com/franciscostanleyvasconceloszelaya/stores/mainModule/model/MainInteract.kt
+++ Stores_MVVM/app/src/main/java/com/franciscostanleyvasconceloszelaya/stores/mainModule/model/MainInteract.kt
@@ -7,6 +7,7 @@ import com.franciscostanleyvasconceloszelaya.stores.StoreApplication
 import com.franciscostanleyvasconceloszelaya.stores.common.entities.StoreEntity
 import com.franciscostanleyvasconceloszelaya.stores.common.utils.Constants
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
@@ -20,8 +21,20 @@ class MainInteract {
 
             val status = response.getInt(Constants.STATUS_PROPERTY)
 
-            if (status == Constants.SUCCESS){
+            if (status == Constants.SUCCESS) {
                 Log.i("status", status.toString())
+//
+//                val jsonObject =
+//                    Gson().fromJson<StoreEntity>(
+//                        response.getJSONArray(Constants.STORES_PROPERTY).get(0).toString(), StoreEntity::class.java
+//                    )
+//                Log.i("storeEntity", jsonObject.toString())
+
+                val jsonList = response.getJSONArray(Constants.STORES_PROPERTY).toString()
+                val mutableListType = object : TypeToken<MutableList<StoreEntity>>(){}.type
+                val storeList = Gson().fromJson<MutableList<StoreEntity>>(jsonList, mutableListType)
+
+                callback(storeList)
             }
         }, {
             it.printStackTrace()
