5c9f51428af64b459c3dbb4f15acd5cbac788811
Frank <stanleyvasconcelos0@gmail.com>
2022-05-19 12:47:18 -0600

    feat(login-api-rest): use of the button with binding

--- LoginAPIREST/app/src/main/java/com/franciscostanleyvasconceloszelaya/loginapirest/MainActivity.kt
+++ LoginAPIREST/app/src/main/java/com/franciscostanleyvasconceloszelaya/loginapirest/MainActivity.kt
@@ -2,10 +2,36 @@ package com.franciscostanleyvasconceloszelaya.loginapirest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.franciscostanleyvasconceloszelaya.loginapirest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.swType.setOnCheckedChangeListener { button, checked ->
+            button.text =
+                if (checked) getString(R.string.main_type_login)
+                else getString(R.string.main_type_register)
+
+            mBinding.btnLogin.text = button.text
+        }
+        mBinding.btnLogin.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login() {
+        updateUI(":)")
+    }
+
+    private fun updateUI(result: String) {
+        mBinding.tvResult.visibility = View.VISIBLE
+        mBinding.tvResult.text = result
     }
 }
