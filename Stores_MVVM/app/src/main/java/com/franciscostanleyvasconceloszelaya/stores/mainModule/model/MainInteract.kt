c7e7f39a142d60dc640500aa04e39fd6f9c027d9
Frank <stanleyvasconcelos0@gmail.com>
2022-05-19 09:14:15 -0600

    feat(stores_mvvm_using_api): check the status of a json with and create constant in order to use in all the program

--- Stores_MVVM/app/src/main/java/com/franciscostanleyvasconceloszelaya/stores/mainModule/model/MainInteract.kt
+++ Stores_MVVM/app/src/main/java/com/franciscostanleyvasconceloszelaya/stores/mainModule/model/MainInteract.kt
@@ -1,16 +1,41 @@
 package com.franciscostanleyvasconceloszelaya.stores.mainModule.model
 
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
 import com.franciscostanleyvasconceloszelaya.stores.StoreApplication
 import com.franciscostanleyvasconceloszelaya.stores.common.entities.StoreEntity
+import com.franciscostanleyvasconceloszelaya.stores.common.utils.Constants
+import com.google.gson.Gson
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
 class MainInteract {
 
     fun getStores(callback: (MutableList<StoreEntity>) -> Unit) {
+        val url = Constants.STORES_URL + Constants.GET_ALL_PATH
+
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, { response ->
+            Log.i("Response", response.toString())
+
+            val status = response.getInt(Constants.STATUS_PROPERTY)
+
+            if (status == Constants.SUCCESS){
+                Log.i("status", status.toString())
+            }
+        }, {
+            it.printStackTrace()
+        })
+
+        StoreApplication.storesAPI.addToRequestQueue(jsonObjectRequest)
+    }
+
+    fun getStoresRoom(callback: (MutableList<StoreEntity>) -> Unit) {
         doAsync {
             val storeList = StoreApplication.database.storeDao().getAllStores()
             uiThread {
+                val json = Gson().toJson(storeList)
+                Log.i("GSON", json)
                 callback(storeList)
             }
         }
