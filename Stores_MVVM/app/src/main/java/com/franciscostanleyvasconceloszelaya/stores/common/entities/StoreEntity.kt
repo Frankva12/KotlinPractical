ad586cbcbaf1e897765b2ef44141e11fd559dd58
Frank <stanleyvasconcelos0@gmail.com>
2022-05-24 09:55:59 -0600

    feat(stores_mvvm): generate an adapter in order to use livedata

--- Stores_MVVM/app/src/main/java/com/franciscostanleyvasconceloszelaya/stores/common/entities/StoreEntity.kt
+++ Stores_MVVM/app/src/main/java/com/franciscostanleyvasconceloszelaya/stores/common/entities/StoreEntity.kt
@@ -15,19 +15,4 @@ data class StoreEntity(
 
 ) {
     constructor() : this(name = "", phone = "", website = "", photoUrl = "")
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as StoreEntity
-
-        if (id != other.id) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return id.hashCode()
-    }
 }
--- Stores_MVVM/app/src/main/java/com/franciscostanleyvasconceloszelaya/stores/mainModule/MainActivity.kt
+++ Stores_MVVM/app/src/main/java/com/franciscostanleyvasconceloszelaya/stores/mainModule/MainActivity.kt
@@ -16,13 +16,14 @@ import com.franciscostanleyvasconceloszelaya.stores.editModule.EditStoreFragment
 import com.franciscostanleyvasconceloszelaya.stores.editModule.viewModel.EditStoreViewModel
 import com.franciscostanleyvasconceloszelaya.stores.mainModule.adapter.OnClickListener
 import com.franciscostanleyvasconceloszelaya.stores.mainModule.adapter.StoreAdapter
+import com.franciscostanleyvasconceloszelaya.stores.mainModule.adapter.StoreListAdapter
 import com.franciscostanleyvasconceloszelaya.stores.mainModule.viewModel.MainViewModel
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity(), OnClickListener {
 
     private lateinit var mBinding: ActivityMainBinding
-    private lateinit var mAdapter: StoreAdapter
+    private lateinit var mAdapter: StoreListAdapter
     private lateinit var mGridLayout: GridLayoutManager
 
     //MVVM
@@ -43,8 +44,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
     private fun setUpViewModel() {
         mMainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
         mMainViewModel.getStores().observe(this) { stores ->
-            mAdapter.setStores(stores)
-//            mBinding.progressBar.visibility = if (stores.isEmpty()) View.VISIBLE else View.GONE
+            mAdapter.submitList(stores)
         }
         mMainViewModel.isShowProgress().observe(this) { isShowProgress ->
             mBinding.progressBar.visibility = if (isShowProgress) View.VISIBLE else View.GONE
@@ -56,9 +56,6 @@ class MainActivity : AppCompatActivity(), OnClickListener {
                 mBinding.fab.show()
             else
                 mBinding.fab.hide()
-        }
-        mEditStoreViewModel.getStoreSelected().observe(this) { storeEntity ->
-            mAdapter.add(storeEntity)
         }
     }
 
@@ -76,7 +73,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
     }
 
     private fun setUpRecyclerView() {
-        mAdapter = StoreAdapter(mutableListOf(), this)
+        mAdapter = StoreListAdapter(this)
         mGridLayout = GridLayoutManager(this, resources.getInteger(R.integer.main_columns))
 
         mBinding.recyclerView.apply {
