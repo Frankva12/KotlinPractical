e0a6f8ed55b12661a9e7f1e99d50f8f6a1826d19
Frank <stanleyvasconcelos0@gmail.com>
2022-05-30 16:05:50 -0600

    feat(coupons): design of the main activity

--- Coupons/app/src/main/java/com/franciscostanleyvasconceloszelaya/coupons/MainActivity.kt
+++ Coupons/app/src/main/java/com/franciscostanleyvasconceloszelaya/coupons/MainActivity.kt
@@ -2,8 +2,10 @@ package com.franciscostanleyvasconceloszelaya.coupons
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.franciscostanleyvasconceloszelaya.coupons.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
