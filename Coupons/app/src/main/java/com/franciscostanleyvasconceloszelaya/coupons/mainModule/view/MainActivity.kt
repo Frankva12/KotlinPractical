b4a3fafadaeaab35c54dde552e57c20a62d2b43b
Frank <stanleyvasconcelos0@gmail.com>
2022-05-30 21:31:44 -0600

    feat(coupons): create and consult coupons using the architecture recommend by android, coroutines and mvvm architecture

--- Coupons/app/src/main/java/com/franciscostanleyvasconceloszelaya/coupons/mainModule/view/MainActivity.kt
+++ Coupons/app/src/main/java/com/franciscostanleyvasconceloszelaya/coupons/mainModule/view/MainActivity.kt
@@ -1,13 +1,68 @@
 package com.franciscostanleyvasconceloszelaya.coupons.mainModule.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.franciscostanleyvasconceloszelaya.coupons.R
+import com.franciscostanleyvasconceloszelaya.coupons.common.entities.CouponEntity
+import com.franciscostanleyvasconceloszelaya.coupons.common.utils.hideKeyBoard
+import com.franciscostanleyvasconceloszelaya.coupons.databinding.ActivityMainBinding
+import com.franciscostanleyvasconceloszelaya.coupons.mainModule.viewModel.MainViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupViewModel()
+        setupObservers()
+        setupButtons()
+    }
+
+
+    private fun setupViewModel() {
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
+    private fun setupObservers() {
+        mainViewModel.getResult().observe(this) { coupon ->
+            if (coupon == null) {
+                binding.tilDescription.hint = getString(R.string.main_hint_description)
+                binding.tilDescription.isEnabled = true
+                binding.btnCreate.visibility = View.VISIBLE
+            } else {
+                binding.etDescription.setText(coupon.description)
+                val status =
+                    getString(if (coupon.isActive) R.string.main_hint_active else R.string.main_hint_description)
+                binding.tilDescription.hint = status
+                binding.tilDescription.isEnabled = false
+                binding.btnCreate.visibility = if (coupon.isActive) View.GONE else View.VISIBLE
+            }
+        }
+        mainViewModel.getSnackBarMsg().observe(this) { msg ->
+            Snackbar.make(binding.root, msg, Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setupButtons() {
+        binding.btnConsult.setOnClickListener {
+            mainViewModel.consultCouponByCode(binding.etCoupon.text.toString())
+            hideKeyBoard(this, binding.root)
+        }
+        binding.btnCreate.setOnClickListener {
+            val coupon = CouponEntity(
+                code = binding.etCoupon.text.toString(),
+                description = binding.etDescription.text.toString()
+            )
+            mainViewModel.saveCoupon(coupon)
+            hideKeyBoard(this, binding.root)
+        }
     }
 }
--- Coupons/app/src/main/java/com/franciscostanleyvasconceloszelaya/coupons/mainModule/viewModel/MainViewModel.kt
+++ Coupons/app/src/main/java/com/franciscostanleyvasconceloszelaya/coupons/mainModule/viewModel/MainViewModel.kt
@@ -1,4 +1,38 @@
 package com.franciscostanleyvasconceloszelaya.coupons.mainModule.viewModel
 
-class MainViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.franciscostanleyvasconceloszelaya.coupons.R
+import com.franciscostanleyvasconceloszelaya.coupons.common.entities.CouponEntity
+import com.franciscostanleyvasconceloszelaya.coupons.common.utils.getMsgErrorByCode
+import com.franciscostanleyvasconceloszelaya.coupons.mainModule.model.MainRepository
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    private val repository = MainRepository()
+
+    private val result = MutableLiveData<CouponEntity>()
+    fun getResult() = result
+
+    private val snackbarMsg = MutableLiveData<Int>()
+    fun getSnackBarMsg() = snackbarMsg
+
+    fun consultCouponByCode(code: String) {
+        viewModelScope.launch {
+            result.value = repository.consultCouponByCode(code)
+        }
+    }
+
+    fun saveCoupon(couponEntity: CouponEntity) {
+        viewModelScope.launch {
+            try {
+                repository.saveCoupon(couponEntity)
+                consultCouponByCode(couponEntity.code)
+                snackbarMsg.value = R.string.main_save_success
+            } catch (e: Exception) {
+                snackbarMsg.value = getMsgErrorByCode(e.message)
+            }
+        }
+    }
 }
