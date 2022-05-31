cddcca2e7b4f315e125cc255761c7c7ac5af10a4
Frank <stanleyvasconcelos0@gmail.com>
2022-05-31 10:48:07 -0600

    feat(coupons): change the text and color base on the status of the coupon

--- Coupons/app/src/main/java/com/franciscostanleyvasconceloszelaya/coupons/common/adapters/CouponBindingAdapter.kt
+++ Coupons/app/src/main/java/com/franciscostanleyvasconceloszelaya/coupons/common/adapters/CouponBindingAdapter.kt
@@ -1,9 +1,23 @@
 package com.franciscostanleyvasconceloszelaya.coupons.common.adapters
 
+import android.graphics.Color
 import android.view.View
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.franciscostanleyvasconceloszelaya.coupons.R
 
 @BindingAdapter("isGone")
 fun bindIsGone(view: View, isGone: Boolean) {
     view.visibility = if (isGone) View.GONE else View.VISIBLE
+}
+
+@BindingAdapter("TextColorChange")
+fun changeTextColor(text: TextView, couponStatus: Boolean) {
+    if (couponStatus) {
+        text.setText(R.string.main_text_title)
+        text.setTextColor(Color.GRAY)
+    } else {
+        text.setText(R.string.main_text_create)
+        text.setTextColor(Color.BLUE)
+    }
 }
