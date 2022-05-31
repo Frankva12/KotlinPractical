25f9b443484b71183058a5510f9b79ccb3ef15d0
Frank <stanleyvasconcelos0@gmail.com>
2022-05-31 08:59:35 -0600

    feat(coupons): create and consult coupons using databinding and make the app multilingual

--- Coupons/app/src/main/java/com/franciscostanleyvasconceloszelaya/coupons/mainModule/view/MainActivity.kt
+++ Coupons/app/src/main/java/com/franciscostanleyvasconceloszelaya/coupons/mainModule/view/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.franciscostanleyvasconceloszelaya.coupons.mainModule.view
 
 import android.os.Bundle
-import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
+import androidx.databinding.DataBindingUtil
+import com.franciscostanleyvasconceloszelaya.coupons.BR
 import com.franciscostanleyvasconceloszelaya.coupons.R
-import com.franciscostanleyvasconceloszelaya.coupons.common.entities.CouponEntity
 import com.franciscostanleyvasconceloszelaya.coupons.common.utils.hideKeyBoard
 import com.franciscostanleyvasconceloszelaya.coupons.databinding.ActivityMainBinding
 import com.franciscostanleyvasconceloszelaya.coupons.mainModule.viewModel.MainViewModel
@@ -14,55 +14,34 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         setupViewModel()
         setupObservers()
-        setupButtons()
     }
 
 
     private fun setupViewModel() {
-        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        val vm: MainViewModel by viewModels()
+        binding.lifecycleOwner = this
+        binding.setVariable(BR.viewModel, vm)
     }
 
     private fun setupObservers() {
-        mainViewModel.getResult().observe(this) { coupon ->
-            if (coupon == null) {
-                binding.tilDescription.hint = getString(R.string.main_hint_description)
-                binding.tilDescription.isEnabled = true
-                binding.btnCreate.visibility = View.VISIBLE
-            } else {
-                binding.etDescription.setText(coupon.description)
-                val status =
-                    getString(if (coupon.isActive) R.string.main_hint_active else R.string.main_hint_description)
-                binding.tilDescription.hint = status
-                binding.tilDescription.isEnabled = false
-                binding.btnCreate.visibility = if (coupon.isActive) View.GONE else View.VISIBLE
+        binding.viewModel?.let {
+            it.coupon.observe(this@MainActivity) { coupon ->
+                binding.isActive = coupon != null && coupon.isActive
             }
-        }
-        mainViewModel.getSnackBarMsg().observe(this) { msg ->
-            Snackbar.make(binding.root, msg, Snackbar.LENGTH_SHORT).show()
-        }
-    }
+            it.getSnackBarMsg().observe(this@MainActivity) { msg ->
+                Snackbar.make(binding.root, msg, Snackbar.LENGTH_SHORT).show()
+            }
+            it.isHideKeyBoard().observe(this@MainActivity) { isHide ->
+                if (isHide) hideKeyBoard(this@MainActivity, binding.root)
 
-    private fun setupButtons() {
-        binding.btnConsult.setOnClickListener {
-            mainViewModel.consultCouponByCode(binding.etCoupon.text.toString())
-            hideKeyBoard(this, binding.root)
-        }
-        binding.btnCreate.setOnClickListener {
-            val coupon = CouponEntity(
-                code = binding.etCoupon.text.toString(),
-                description = binding.etDescription.text.toString()
-            )
-            mainViewModel.saveCoupon(coupon)
-            hideKeyBoard(this, binding.root)
+            }
         }
     }
 }
--- Coupons/app/src/main/java/com/franciscostanleyvasconceloszelaya/coupons/mainModule/viewModel/MainViewModel.kt
+++ Coupons/app/src/main/java/com/franciscostanleyvasconceloszelaya/coupons/mainModule/viewModel/MainViewModel.kt
@@ -12,26 +12,38 @@ import kotlinx.coroutines.launch
 class MainViewModel : ViewModel() {
     private val repository = MainRepository()
 
-    private val result = MutableLiveData<CouponEntity>()
-    fun getResult() = result
+    val coupon = MutableLiveData(CouponEntity())
+
+    private val hideKeyBoard = MutableLiveData<Boolean>()
+    fun isHideKeyBoard() = hideKeyBoard
 
     private val snackbarMsg = MutableLiveData<Int>()
     fun getSnackBarMsg() = snackbarMsg
 
-    fun consultCouponByCode(code: String) {
-        viewModelScope.launch {
-            result.value = repository.consultCouponByCode(code)
+    fun consultCouponByCode() {
+        coupon.value?.code?.let { code ->
+            viewModelScope.launch {
+                hideKeyBoard.value = true
+                coupon.value = repository.consultCouponByCode(code) ?: CouponEntity(
+                    code = code,
+                    isActive = false
+                )
+            }
         }
     }
 
-    fun saveCoupon(couponEntity: CouponEntity) {
-        viewModelScope.launch {
-            try {
-                repository.saveCoupon(couponEntity)
-                consultCouponByCode(couponEntity.code)
-                snackbarMsg.value = R.string.main_save_success
-            } catch (e: Exception) {
-                snackbarMsg.value = getMsgErrorByCode(e.message)
+    fun saveCoupon() {
+        coupon.value?.let { couponEntity ->
+            viewModelScope.launch {
+                hideKeyBoard.value = true
+                try {
+                    couponEntity.isActive = true
+                    repository.saveCoupon(couponEntity)
+                    consultCouponByCode()
+                    snackbarMsg.value = R.string.main_save_success
+                } catch (e: Exception) {
+                    snackbarMsg.value = getMsgErrorByCode(e.message)
+                }
             }
         }
     }
