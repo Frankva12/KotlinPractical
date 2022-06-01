1aa5c51b51c75e9a2775f3a39e09342d5640b370
Frank <stanleyvasconcelos0@gmail.com>
2022-06-01 09:56:10 -0600

    feat(coupons): using of espresso to make tests

--- Coupons/app/src/androidTest/java/com/franciscostanleyvasconceloszelaya/coupons/MainActivityCreateTest.kt
+++ Coupons/app/src/androidTest/java/com/franciscostanleyvasconceloszelaya/coupons/MainActivityCreateTest.kt
@@ -9,6 +9,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.franciscostanleyvasconceloszelaya.coupons.mainModule.view.MainActivity
+import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -24,7 +25,7 @@ class MainActivityCreateTest {
         val etCoupon = onView(withId(R.id.etCoupon))
         etCoupon.check(matches(withText("")))//verifica que(view) coincida(con el texto("")))
         etCoupon.perform(click())
-        etCoupon.perform(replaceText("Welcome_01"))
+        etCoupon.perform(replaceText("WELCOME_02"))
 
         val btnConsult = onView(withId(R.id.btnConsult))
         btnConsult.perform(click())
@@ -35,6 +36,31 @@ class MainActivityCreateTest {
         val etDescription = onView(withId(R.id.etDescription))
         etDescription.perform(replaceText("Cupon1"))
 
+
+        etDescription.perform(replaceText("WELCOME_01"))
         btnCreate.perform(click())
+
+        val snackbar = onView(withId(com.google.android.material.R.id.snackbar_text))
+        snackbar.check(matches(withText("Created coupon")))
+    }
+
+    /*
+    * Corrobora que el boton "crear" no existe y no es visible.
+    * Test: nuesto etCoupon inicia vacio, luego haz click sobre el, añade el texto "WELCOME_01"
+    * y ahora desde btnConsult, haz click sobre el.
+    * Verifica que el btnCreate no es visible
+    * */
+    @Test
+    fun consultCouponExistTest() {
+        val etCoupon = onView(withId(R.id.etCoupon))
+        etCoupon.check(matches(withText("")))
+        etCoupon.perform(click())
+        etCoupon.perform(replaceText("WELCOME_01"))
+
+        val btnConsult = onView(withId(R.id.btnConsult))
+        btnConsult.perform(click())
+
+        val btnCreate = onView(withId(R.id.btnCreate))
+        btnCreate.check(matches(not(isDisplayed())))// ! = not()
     }
 }
