d5b7ae59e38852fb137fc5c349d53494a6f550a4
Frank <stanleyvasconcelos0@gmail.com>
2022-06-07 14:50:10 -0600

    feat(weather): add the repository for the data in the main module

---
+++ Weather/app/src/main/java/com/franciscostanleyvasconceloszelaya/weather/mainModule/model/MainRepository.kt
@@ -0,0 +1,16 @@
+package com.franciscostanleyvasconceloszelaya.weather.mainModule.model
+
+import com.franciscostanleyvasconceloszelaya.weather.common.entities.WeatherForecastEntity
+
+class MainRepository {
+    private val remoteDatabase = RemoteDatabase()
+
+    suspend fun getWeatherAndForecast(
+        lat: Double,
+        long: Double,
+        appId: String,
+        units: String,
+        lang: String
+    ): WeatherForecastEntity =
+        remoteDatabase.getWeatherForecastByCoordinates(lat, long, appId, units, lang)
+}
