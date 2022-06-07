79ad866ba9738a87cf050418df4efe5e3e841a16
Frank <stanleyvasconcelos0@gmail.com>
2022-06-07 12:34:00 -0600

    feat(weather): add the service for using the API

---
+++ Weather/app/src/main/java/com/franciscostanleyvasconceloszelaya/weather/common/dataAccess/WeatherService.kt
@@ -0,0 +1,17 @@
+package com.franciscostanleyvasconceloszelaya.weather.common.dataAccess
+
+import com.franciscostanleyvasconceloszelaya.weather.common.WeatherForecastEntity
+import com.franciscostanleyvasconceloszelaya.weather.common.utils.Constants
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherService {
+    @GET(Constants.ONE_CALL_PATH)
+    suspend fun getWeatherForecastByCoordinates(
+        @Query(Constants.LATITUDE_PARAM) lat: Double,
+        @Query(Constants.LONGITUDE_PARAM) lon: Double,
+        @Query(Constants.APP_ID_PARAM) appId: String,
+        @Query(Constants.UNITS_PARAM) units: String,
+        @Query(Constants.LANGUAGE_PARAM) lang: String
+    ): WeatherForecastEntity
+}
---
+++ Weather/app/src/main/java/com/franciscostanleyvasconceloszelaya/weather/common/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.franciscostanleyvasconceloszelaya.weather.common.utils
+
+object Constants {
+    const val BASE_URL = "https://api.openweathermap.org/"
+    const val ONE_CALL_PATH = "data/2.5/onecall"
+
+    const val LATITUDE_PARAM = "lat"
+    const val LONGITUDE_PARAM = "lon"
+    const val APP_ID_PARAM = "appid"
+    const val UNITS_PARAM = "units"
+    const val LANGUAGE_PARAM = "lang"
+}
